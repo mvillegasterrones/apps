292bac48454b971f8d8625412d6325ab337298d9
Manuel Villegas <34917540+mvillegasterrones@users.noreply.github.com>
2024-03-07 14:56:15 -0500

    upd-safari

--- ventas_safari/index.php
+++ ventas_safari/index.php
@@ -22,7 +22,6 @@
     <script src="css_master/jquery.bgiframe-2.1.2.js"></script>
     <script src="css_master/jquery-ui.js"></script>
     <link rel="stylesheet" href="css_master/layout.css" />
-     
 </head>
  <body style="background:#ddd;">
         <div id="empresa"  style="border: 1px solid; background-color: #8CF9CC; border-bottom: 3px solid; border-right: 3px solid; margin-top: 150px; margin-left: 470px; width: 400px; height: 310px"> 
@@ -38,7 +37,6 @@
            <center><input type="submit" class="button white" value="Seleccionar" id="seleccionar" name="seleccionar"></center>
            <br>
         </form>
-            
-    </div>    
+    </div>
    </body> 
 </html>
