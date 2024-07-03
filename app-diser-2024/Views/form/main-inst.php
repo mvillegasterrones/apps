841862f1b9210ff2ee3dc6f2033b0129d0f34f50
Manuel Villegas <34917540+mvillegasterrones@users.noreply.github.com>
2024-07-03 13:01:49 -0500

    init-emc-03

--- app-diser-2024/Views/form/main-inst.php
+++ app-diser-2024/Views/form/main-inst.php
@@ -246,4 +246,38 @@
         </div>
     </div>
 
+    <div class="col-lg-3 col-md-6 col-12 mt-4 mt-lg-0">
+        <div class="card bg-primary cursor-pointer">
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-white text-sm mb-0 text-uppercase font-weight-bold opacity-7">
+                                RECEPCIÓN DE KITS DE BISNESTAR
+                            </p>
+                            <h5 class="text-white font-weight-bolder mb-0">ENC-03</h5>
+                            <hr class="horizontal light mt-1 mb-3" />
+
+                            <button type="button" class="btn btn-xs btn-info mb-0" data-bs-toggle="modal"
+                                data-bs-target="#" onclick="funciones().load_form_content('form-enc-02.php')"><i
+                                    class="fa-regular fa-plus"></i> Nuevo</button>
+
+                            <button type="button" class="btn btn-xs btn-info mb-0" data-bs-toggle="modal"
+                                data-bs-target="#modal-form-enc-02" onclick="encuesta_02().get_reporte()"><i
+                                    class="fa-duotone fa-files"></i> Reporte</button>
+
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div class="icon icon-shape bg-white shadow text-center rounded-circle">
+                            <i class="fa-duotone fa-sparkles text-dark text-lg opacity-10"
+                                aria-hidden="true"></i>
+                            <!--<i class="ni ni-note-03 text-dark text-lg opacity-10" aria-hidden="true"></i>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
