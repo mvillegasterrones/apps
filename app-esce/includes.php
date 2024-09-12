df24e87b3be3d2a39f8f7b983ec9d1d0afc118ab
Manuel Villegas <34917540+mvillegasterrones@users.noreply.github.com>
2024-09-12 17:45:49 -0500

    upd-all-day-12.09

---
+++ app-esce/includes.php
@@ -0,0 +1,96 @@
+<?php
+
+$ico = './assets/img/esce.png';
+$logo = './assets/img/logos/esce-full.png';
+
+$social_facebook      = '#';
+$social_instagram     = '#';
+$social_linkedin      = '#';
+
+$nombre_empresa       = 'ECEGP';
+$correo_empresa       = 'consultas@ecegp.edu.pe';
+$nombre_largo_empresa = 'Escruela de Ciencias Empresariales y Gestión Publica';
+$web_url_design       = 'https://mvillegasterrones.github.io/apps/app-my-profile/index.html';
+
+$img_hero_01    = './assets/img/hero/hero-001.jpg';
+$img_hero_02    = './assets/img/hero/hero-002.jpg';
+$img_hero_03    = './assets/img/hero/hero-003.jpg';
+$img_hero_04    = './assets/img/hero/hero-crecimiento-empresarial-01.jpg';
+$img_hero_05    = './assets/img/hero/hero-capacitacion-continua.jpg';
+
+
+$img_hero_teaching_01 = './img/hero/theaching-teachers-01.jpg';
+$img_hero_teaching_02 = './img/hero/theaching-teachers-02.jpg';
+
+$img_hero_capacitac_continua = './img/hero/hero-capacitacion-continua.jpg';
+
+$img_hero_reskalling_01 = 'https://www.michaelpage.es/sites/michaelpage.es/files/legacy/upskilling_article_970x388d8.jpg';
+$img_hero_reskalling_02 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0B-3UpcFU5T1F2aNInwGFqgx7fmMDA_kRpqRM5-qMYif1gzv6VmrEFodj5e89W1nC1TA&usqp=CAU';
+
+$img_valores_01 = './img/valores/valores-001.jpg';
+$img_valores_02 = './img/valores/valores-002.jpg';
+$img_valores_03 = './img/valores/valores-003.jpg';
+$img_valores_04 = './img/valores/valores-004.jpg';
+$img_valores_05 = './img/valores/valores-005.jpg';
+$img_valores_06 = './img/valores/valores-006.jpg';
+$img_valores_07 = './img/valores/valores-007.jpg';
+
+$cert_img_card_1 = './img/cards/img-card-01.jpeg';
+$cert_img_card_2 = './img/cards/img-card-02.jpg';
+$cert_img_card_3 = './img/cards/img-card-03.jpeg';
+
+/* ! Menu */
+$innovacion_botom_up      = './innovacion-bottom-up.php';
+$teaching_the_teachers    = './teaching-the-teachers.php';
+$economia_periodistas     = './economia-economistas.php';
+$formacion_emprendedores  = './formacion-emprendedores.php';
+$economia_circular        = './economia-circular.php';
+$modelo_aprendizaje       = './modelo-aprendizaje.php';
+$capacitacion_continua    = './capacitacion-continua.php';
+$re_skalling              = './re-skalling.php';      
+
+$url_img_certificado      = './img/certificate.webp';
+
+/* Main */
+$url_img_socio_guido = 'https://mdbootstrap.com/img/new/avatars/22.jpg';
+$url_img_socio_jose  = 'https://mdbootstrap.com/img/new/avatars/22.jpg';
+
+# -- Imagenes
+$img_econom_perdiodistas  = './img/cards/ttteachers_01.jpeg';
+$precio_econom_perdistas  = '1.00';
+
+$img_formac_emprendedores = './img/cards/ttteachers_03.jpeg';
+$precio_formac_emprended  = '2.00';
+
+$img_econom_circular      = './img/cards/ttteachers_03.jpeg';
+$precio_econom_circular   = '3.00';
+
+$img_econom_docentes      = './img/cards/ttteachers_04.jpeg';
+
+/* ! Crecimiento Empresarial */
+$crecim_empresarial_url         = './crecimiento-empresarial.php';
+$crecim_empresarial_title       = $nombre_empresa . ' | ' . 'Nuestros Cursos';
+$crecim_empresarial_img_hero    = './img/hero/hero-crecimiento-empresarial-01.jpg';
+
+$crecim_empresarial_link_c1     = './innovacion-bottom-up.php';
+$crecim_empresarial_link_img_c1 = 'https://innovationcloud.com/pub/blog/15573097067947_innovation-cloud-how-to-approach-bottom-up-innovation.jpg';
+
+$crecim_empresarial_link_c2     = './proyectos-de-innocacion-empresarial.php';
+$crecim_empresarial_link_img_c2 = 'https://www.businessconsulting.cl/wp-content/uploads/2020/06/maestria_en_gestion_de_la_innovacion_2.jpg';
+
+$crecim_empresarial_link_c3     = './sprints-problem-focuses-workshop.php';
+$crecim_empresarial_link_img_c3 = 'https://miro.medium.com/v2/resize:fit:2000/1*K5M838xIsExaptfzp9E3Zw.png';
+
+/* * CERTIFICACIONES */
+
+$cert_link_card_1   = './certificacion-marketing-digital.php';
+$cert_link_card_2   = './certificacion-transformacion-digital.php';
+$cert_link_card_3   = './certificacion-nuevos-negocios.php';
+
+$cert_precio_card_1 = '4,500.00';
+$cert_precio_card_2 = '4,500.00';
+$cert_precio_card_3 = '4,500.00';
+
+/* * FIN CERTIFICACIONES */
+
+/** RESKALLING */
